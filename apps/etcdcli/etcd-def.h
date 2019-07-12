8b2d5ecef7755801be3bd9f80568cbdd28ac5afb
tangzhenquan <409044065@qq.com>
2019-07-12 19:55:40 +0800

    add etcdctl

---
+++ apps/etcdcli/etcd-def.h
@@ -0,0 +1,38 @@
+//
+// Created by tom on 19-7-12.
+//
+
+#ifndef SAFE_EVPP_ETCD_DEF_H
+#define SAFE_EVPP_ETCD_DEF_H
+
+#include <string>
+#include <stdint.h>
+
+
+namespace etcdcli{
+
+    struct etcd_response_header {
+        uint64_t cluster_id;
+        uint64_t member_id;
+        int64_t revision;
+        uint64_t raft_term;
+    };
+
+    struct etcd_key_value {
+        std::string key;
+        int64_t create_revision;
+        int64_t mod_revision;
+        int64_t version;
+        std::string value;
+        int64_t lease;
+    };
+
+    struct etcd_watch_event {
+        enum type {
+            EN_WEVT_PUT = 0,   // put
+            EN_WEVT_DELETE = 1 // delete
+        };
+    };
+}
+
+#endif //SAFE_EVPP_ETCD_DEF_H
---
+++ apps/etcdcli/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by tom on 19-7-12.
+//
+
+#include "etcd-def.h"
+#include "rapidjson/document.h"
+
+int main() {
+    etcdcli::etcd_response_header header;
+    printf("sdasdasd:%ld \n", header.revision);
+}
